6891332ce8904e84dbcb7cee24fe7d2ce3244b2f
Scott A. Matthews <scott@designsbysm.com>
2025-06-05 12:35:38 -0700

    refactor fetchRemotes to use multiple threads

--- fetch_remotes.go
+++ fetch_remotes.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"os/exec"
+	"sync"
 	"time"
 
 	"github.com/briandowns/spinner"
+	"github.com/designsbysm/timber/v2"
 )
 
+func fetch(path string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	cmd := exec.Command("git", "fetch", "--all", "--prune", "--tags", "--force")
+	cmd.Dir = path
+
+	if err := cmd.Run(); err != nil {
+		timber.Error(err)
+	}
+}
+
 func fetchRemotes(folders []string) error {
-	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
-	// s.Prefix = "Fetching "
-	s.UpdateCharSet(spinner.CharSets[11])
+	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	s.Start()
 
-	for _, path := range folders {
-		cmd := exec.Command("git", "fetch", "--all", "--prune", "--tags", "--force")
-		cmd.Dir = path
-		err := cmd.Run()
+	var wg sync.WaitGroup
 
-		if err != nil {
-			return err
-		}
+	for _, path := range folders {
+		wg.Add(1)
+		go fetch(path, &wg)
 	}
 
+	wg.Wait()
 	s.Stop()
+
 	return nil
 }
--- main.go
+++ main.go
@@ -37,7 +37,5 @@ func main() {
 		return
 	}
 
-	// timber.Debug("Statuses", statuses)
-
 	outputStatuses(statuses)
 }
